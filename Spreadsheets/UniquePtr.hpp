5544049364465b6ef2e9fb83a6806eb1886891e5
Venelina Andreeva <venian2003@abv.bg>
2023-05-24 16:29:30 +0300

    Add unique pointer

---
+++ Spreadsheets/UniquePtr.hpp
@@ -0,0 +1,83 @@
+#pragma warning
+
+template <typename T>
+class UniquePtr {
+private:
+	T* ptr = nullptr;
+public:
+	UniquePtr() = default;
+	UniquePtr(const T* ptr);
+
+	UniquePtr(const UniquePtr& other) = delete;
+	UniquePtr& operator=(const UniquePtr& other) = delete;
+
+	UniquePtr(UniquePtr&& other) noexcept;
+	UniquePtr& operator=(UniquePtr&& other) noexcept;
+
+	~UniquePtr();
+
+private:
+	void MoveFrom(UniquePtr&& other);
+	void Free();
+public:
+	T& operator*();
+	T operator*() const;
+
+	operator bool() const;
+	bool operator!() const;
+};
+
+template <typename T>
+UniquePtr<T>::UniquePtr<T>(const T* ptr) {
+	this->ptr = ptr;
+}
+
+template <typename T>
+UniquePtr<T>::UniquePtr(UniquePtr&& other) noexcept {
+	MoveFrom(std::move());
+}
+
+template <typename T>
+UniquePtr<T>& UniquePtr<T>::operator=(UniquePtr&& other) noexcept {
+	if (this != &other) {
+		Free();
+		MoveFrom(std::move(other));
+	}
+	return *this;
+}
+
+template <typename T>
+UniquePtr<T>::~UniquePtr() {
+	Free();
+}
+
+template <typename T>
+void UniquePtr<T>::MoveFrom(UniquePtr&& other) {
+	this->ptr = other.ptr;
+	other.ptr = nullptr;
+}
+
+template <typename T>
+void UniquePtr<T>::Free() {
+	delete ptr;
+}
+
+template <typename T>
+T& UniquePtr<T>::operator*() {
+	return *this->ptr;
+}
+
+template <typename T>
+T UniquePtr<T>::operator*() const {
+	return *this->ptr;
+}
+
+template <typename T>
+UniquePtr<T>::operator bool() const {
+	return (this->ptr != nullptr);
+}
+
+template <typename T>
+bool UniquePtr<T>::operator!() const {
+	return (mPointer == nullptr);
+}
